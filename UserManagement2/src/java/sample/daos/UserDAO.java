a995e7d3a462756dd236041d04623ef49345015e
Nguyễn Trí Nhân <55549681+nhanse@users.noreply.github.com>
2020-05-26 15:20:40 +0700

    new project

---
+++ UserManagement2/src/java/sample/daos/UserDAO.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.daos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import sample.dtos.UserDTO;
+import sample.utils.DBUtils;
+
+/**
+ *
+ * @author nguyentrinhan2000
+ */
+public class UserDAO {
+
+    public String checkLogin(String userID, String password) throws SQLException {
+        String result = "";
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                String sql = "SELECT userID FROM tbUser "
+                        + "WHERE userID= " + "'" + userID + "'" + " AND password = " + "'" + password + "'";
+                stm = conn.prepareStatement(sql);
+//                stm.setString(1, userID);
+//                stm.setString(2, password);
+                rs = stm.executeQuery();
+                if (rs.next()) {
+                    result = rs.getString("fullName");
+                }
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return result;
+    }
+    public List<UserDTO> getListUser(String search) throws SQLException{
+        List<UserDTO> result = new ArrayList<UserDTO>();
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                String sql = "SELECT userID, fullName FROM tbUser"
+                        + " WHERE fullName like ?";
+                stm = conn.prepareStatement(sql);
+                stm.setString(1, "%"+search+"%");
+                rs = stm.executeQuery();
+                while (rs.next()){
+                    String userID = rs.getNString("userID");
+                    String fullName = rs.getNString("fullName");
+                    String password = "***";
+                    String roleID = rs.getNString("roleID");
+                    result.add(new UserDTO(userID, fullName, password, roleID));
+                }
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) rs.close();
+            if (stm != null) stm.close();
+            if (conn != null) conn.close();
+        }
+        return result;
+    }
+}
